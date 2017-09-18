bcfb755a21c93c16b4c28bd27d0e4ba2ef00bc57
Mutasem Halawani <MutasemH@Nli.loc>
2017-09-18 17:20:16 +0300

    rebuild project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,38 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-// import { HeroDetailComponent } from './hero-detail.component';
+import {NgForm} from '@angular/forms';
+
+export class Hero{
+    id:number;
+    name: string;
+}
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
 ];
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl:'./app.component.html', 
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'Tour of Heros';
-  heroes = HEROES;
-  selectedHero: Hero;
+export class AppComponent{
+    title = "Tour of Heroes";
 
-  onSelect(hero: Hero):void{
-    this.selectedHero = hero;
-  }
+    selectedHero: Hero;
+    
+    heroes = HEROES;
+
+    onSelect(hero: Hero): void{
+        this.selectedHero = hero;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+// import { HeroDetailComponent } from './hero-detail.component';
+// import { HeroesComponent } from './heroes.component';
+
+// import { HeroService } from './hero.service';
+
+const appRoutes: Routes =[
+  // { 
+  //   path: 'crisis-center',
+  //   component: CrisisListComponent
+  // },
+  // {
+  //   path: 'hero/:id', 
+  //   component: HeroDetailComponent
+  // },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data:{
+  //          title: 'Heroes List'
+  //        }  
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent 
+  // }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent,
+    // HeroDetailComponent,
+    // HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    // RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
-  providers: [],
+  // providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
