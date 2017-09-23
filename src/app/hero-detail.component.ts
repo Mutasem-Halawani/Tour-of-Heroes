f4d94d7c26c56c1914af22ffac0f4563924a60b9
mutasem <mutasem.halawani@gmail.com>
2017-09-23 12:57:13 +0300

    finish project

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -9,36 +9,7 @@ import { Hero } from './hero';
 @Component({
     selector:'hero-detail',
     templateUrl:'./hero-detail.component.html',
-    styles: [`
-    label {
-        display: inline-block;
-        width: 3em;
-        margin: .5em 0;
-        color: #607D8B;
-        font-weight: bold;
-      }
-      input {
-        height: 2em;
-        font-size: 1em;
-        padding-left: .4em;
-      }
-      button {
-        margin-top: 20px;
-        font-family: Arial;
-        background-color: #eee;
-        border: none;
-        padding: 5px 10px;
-        border-radius: 4px;
-        cursor: pointer; cursor: hand;
-      }
-      button:hover {
-        background-color: #cfd8dc;
-      }
-      button:disabled {
-        background-color: #eee;
-        color: #ccc; 
-        cursor: auto;
-      }`]
+    styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent{
@@ -60,6 +31,11 @@ export class HeroDetailComponent{
 
     goBack(): void{
         this.location.back();
+    }
+
+    save(): void{
+        this.heroService.update(this.hero)
+            .then(()=> this.goBack());
     }
 
 }
